a823eedc80e6544b01547a0e3acf5cda84c50cd3
wandera7 <julius.wandera@student.moringaschool.com>
2023-02-22 19:58:53 +0300

    Initial app

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('My players'),
+        ),
+        body: Column(
+          children: [Container(
+
+          )],
+        ));
+  }
+}
